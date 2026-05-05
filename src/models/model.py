8965aecedeab40ddf38d5b519581e9097cc6fbc1
noelng <noelngfnz@gmail.com>
2026-05-05 14:24:24 +0800

    added .env.example and changed the model to gemini instead of gemma3

--- src/models/model.py
+++ src/models/model.py
@@ -5,15 +5,15 @@ from langchain_ollama import ChatOllama
 load_dotenv()
 
 # chat model initialization
-# llm_model = ChatGoogleGenerativeAI(
-#     model="gemini-2.5-flash",
-#     temperature=0
-# )
-
-llm_model = ChatOllama(
-    model="gemma3:1b",
+llm_model = ChatGoogleGenerativeAI(
+    model="gemini-3-flash-preview",
     temperature=0
 )
+
+# llm_model = ChatOllama(
+#     model="gemma3:1b",
+#     temperature=0
+# )
 
 # embedding model initialization
 embed_model = GoogleGenerativeAIEmbeddings(
